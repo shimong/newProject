4626c634397dcd6a875685612af7b39c92885df8
shimi greenshtain <shimi.g94@gmail.com>
2018-09-20 08:23:27 +0300

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'geeting-started-ng5';
+  title = 'post board';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoteComponent } from './note/note.component';
+import { NoteListComponent } from './note-list/note-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule
